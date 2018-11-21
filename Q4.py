ca2a0bb5e4b509d797a15eb067a924d908dbb831
Barry Sheppard <barrysheppard@gmail.com>
2018-11-21 16:06:25 +0000

    Commented out code sippets

--- Q4.py
+++ Q4.py
@@ -34,38 +34,38 @@ if __name__ == '__main__':
 # Sample code is below here. Delete before submitting
 ###############################################################################
 
-# Example of map
-a = [1, 2, 3, 4]
-b = [17, 12, 11, 10]
-c = [-1, -4, 5, 9]
-result = map(lambda x, y: x+y, a, b)
+# # Example of map
+# a = [1, 2, 3, 4]
+# b = [17, 12, 11, 10]
+# c = [-1, -4, 5, 9]
+# result = map(lambda x, y: x+y, a, b)
+# #
+# print(list(result))
 #
-print(list(result))
-
-# Example of filter
-fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-result = filter(lambda x: x % 2, fib)
-print(list(result))
-
-# Example of reduce
-# Note if you're using python3 (which we are) and a reduce function you need
-# to import reduce from functools. Best practise is include import at top of
-# code with all the other imports
-
-from functools import reduce
-result = reduce(lambda x, y: x+y, [47, 11, 42, 13])
-print(result)
-
-# List comprehension is similar but different format.
-
-# Example of a map
-celsius = [10, 20, 30]
-fahrenheit = [(float()/5*x + 32) for x in celsius]
-
-# Example of a filter
-pythagorean_triples = [(x, y, z) for x in range(1,30) for y in
-                       range(x,30) for z in range(y,30) if x**2 + y**2 == z**2]
-
-# Example of a reduce
-numbers = list(range(10))
-diff = sum([numbers[0]] + [-x for x in numbers[1:]])
+# # Example of filter
+# fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# result = filter(lambda x: x % 2, fib)
+# print(list(result))
+#
+# # Example of reduce
+# # Note if you're using python3 (which we are) and a reduce function you need
+# # to import reduce from functools. Best practise is include import at top of
+# # code with all the other imports
+#
+# from functools import reduce
+# result = reduce(lambda x, y: x+y, [47, 11, 42, 13])
+# print(result)
+#
+# # List comprehension is similar but different format.
+#
+# # Example of a map
+# celsius = [10, 20, 30]
+# fahrenheit = [(float()/5*x + 32) for x in celsius]
+#
+# # Example of a filter
+# pythagorean_triples = [(x, y, z) for x in range(1,30) for y in
+#                        range(x,30) for z in range(y,30) if x**2 + y**2 == z**2]
+#
+# # Example of a reduce
+# numbers = list(range(10))
+# diff = sum([numbers[0]] + [-x for x in numbers[1:]])
